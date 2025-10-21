10add9be1c0a0250f2a0a8053694ee62a298b0bc
samyraj19 <samyraj.github@gmail.com>
2025-10-21 11:50:42 +0530

    Create AppSession.cs

---
+++ KBilling/Services/AppSession.cs
@@ -0,0 +1,25 @@
+using System;
+using KBilling.Model;
+
+namespace KBilling.Services {
+   public static class AppSession {
+      private static UserModel? mCurrentUser;
+
+      public static UserModel? CurrentUser {
+         get => mCurrentUser;
+         private set {
+            mCurrentUser = value;
+            RoleChanged?.Invoke (null, EventArgs.Empty); // notify everyone
+         }
+      }
+
+      public static bool IsLoggedIn => CurrentUser != null;
+      public static EUserRoles? Role => CurrentUser?.Role;
+
+      public static void Login (UserModel user) => CurrentUser = user;
+      public static void Logout () => CurrentUser = null;
+
+      // Event triggered when role changes
+      public static event EventHandler? RoleChanged;
+   }
+}
