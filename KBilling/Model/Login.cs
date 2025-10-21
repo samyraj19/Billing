ade040204628988eb4390d47bfe0dd6d3b9d218c
samyraj19 <samyraj.github@gmail.com>
2025-10-21 11:49:36 +0530

    Update Login.cs

--- KBilling/Model/Login.cs
+++ KBilling/Model/Login.cs
@@ -1,1 +1,10 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KBilling.Model {
+   public partial class LoginModel : ObservableObject{
+      [ObservableProperty] private string? user;
+      [ObservableProperty] private string? password = "password";
+      [ObservableProperty] private bool? rememberMe;
+   }
+}
 
